d4d507c3f33c1edc2560de7db8196c12b6615a18
Pedro Antunes <pedronmantunes@gmail.com>
2022-11-29 03:24:57 +0000

    [shell.nix] Risc-V toolchain corrected

--- shell.nix
+++ shell.nix
@@ -1,4 +1,4 @@
-{ pkgs ? (import <nixpkgs> {}).pkgsCross.riscv64-embedded }:
+{ pkgs ? (import <nixpkgs> {}).pkgsCross.riscv64 }:
   pkgs.mkShell {
     # nativeBuildInputs is usually what you want -- tools you need to run
     nativeBuildInputs = [ pkgs.buildPackages.dtc ];
