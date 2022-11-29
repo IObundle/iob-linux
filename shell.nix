828985f2747f8f649e7b34606ae8616dea5e7727
Pedro Antunes <pedronmantunes@gmail.com>
2022-11-29 03:02:02 +0000

    [shell.nix] Added riscv64 gcc compiler to nix-shell

--- shell.nix
+++ shell.nix
@@ -1,4 +1,4 @@
-{ pkgs ? import <nixpkgs> {} }:
+{ pkgs ? (import <nixpkgs> {}).pkgsCross.riscv64-embedded }:
   pkgs.mkShell {
     # nativeBuildInputs is usually what you want -- tools you need to run
     nativeBuildInputs = [ pkgs.buildPackages.dtc ];
