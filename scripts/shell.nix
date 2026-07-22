91681cb00d7a7a29e96c4f2636f1f389065eb97a
Artur Nóbrega <arturnobrega1999@gmail.com>
2026-07-23 00:38:14 +0100

    fix(nix): Add missing buildroot dependency

--- scripts/shell.nix
+++ scripts/shell.nix
@@ -11,10 +11,13 @@ pkgs.mkShell {
     ncurses
     # Buildroot
     bc
+    libxcrypt
   ];
   shellHook = ''
     # fixes libstdc++.so.6 issues of buildroot's patchelf
     export LD_LIBRARY_PATH=${pkgs.stdenv.cc.cc.lib}/lib
+    # fixes libxcrypt missing library issues
+    export LD_LIBRARY_PATH=${pkgs.libxcrypt}/lib:$LD_LIBRARY_PATH
   '';
 
   # Disable hardening flags for gcc. Prevents errors when running buildroot, like these:
